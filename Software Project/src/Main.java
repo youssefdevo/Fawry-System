e810ae5835b029d820491608f3ba062e93d1ccf6
Youssef Nasser <y556217@gmail.com>
2022-12-03 00:11:11 +0200

    test

--- Software Project/src/Main.java
+++ Software Project/src/Main.java
@@ -4,8 +4,7 @@ public class Main {
 	 public static void main(String[] args) {
 		 Scanner in = new Scanner(System.in);
 		 int n = in.nextInt();
-		 int t;
-		 int xx;
+		 String test;
 		 
 		 
 	 }
