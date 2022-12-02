d675448fc221a1b0c1795cb2d3b64fafc6908711
Youssef Nasser <y556217@gmail.com>
2022-12-02 23:30:12 +0200

    Test After make repo private.

--- Software Project/src/Main.java
+++ Software Project/src/Main.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 
 public class Main {
 	 public static void main(String[] args) {
-		 int n;
-		 int x;
+		 Scanner in = new Scanner(System.in);
+		 int n = in.nextInt();
+		 
 	 }
 }
