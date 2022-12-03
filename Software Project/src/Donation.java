afd9b1b383fce847a92459da42b9dc6440f60089
Youssef Nasser <y556217@gmail.com>
2022-12-03 20:29:03 +0200

    donation

--- Software Project/src/Donation.java
+++ Software Project/src/Donation.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class Donation implements Service {
 	private Scanner in  = new Scanner(System.in);
-	private Provider provider ;
+	private DonationPlace place;
 	public void mobileRecharageForm()
 	{
 		System.out.println("1- Schools.\n 2- NGOs. \n 3- Hospitals.");
 		int request = in.nextInt();
 		if(request==1)
 		{
-			provider=new We_Mobile();
-			provider.schoolForm();
+			place=new School();
+			place.schoolForm();
 		}
 		else if(request==2)
 		{
-			provider=new Etisalat_Mobile();
-			provider.NGOForm();
+			place=new NGOs();
+			place.NGOForm();
 		}
 		else if(request==3)
 		{
-			provider=new Orange_Mobile();
-			provider.hospitalsForm();
+			place=new Hospital();
+			place.hospitalsForm();
 		}
 	
 		
